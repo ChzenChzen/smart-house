b92b4e1465b621ab2e59459d294807bb9bfb5bde
ChzenChzen <fedorovdanila@me.com>
2022-06-21 14:57:40 +0800

    refactor device

--- src/device.rs
+++ src/device.rs
@@ -1,39 +1,66 @@
-use crate::device_info::DeviceInfo;
+use crate::device_info::{impl_device_info, DeviceInfo};
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Device {
+pub enum DeviceKind {
+    Socket(Socket),
+    Thermo(Thermo),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Socket {
     name: String,
+    state: bool,
 }
 
-impl DeviceInfo for Device {
-    fn name(&self) -> &str {
-        self.name()
+impl Socket {
+    pub fn new(name: impl AsRef<str>, state: bool) -> Self {
+        Self {
+            name: name.as_ref().to_owned(),
+            state,
+        }
     }
+}
 
-    fn state(&self) -> &str {
-        "on"
+#[derive(Debug, Clone, PartialEq)]
+pub struct Thermo {
+    name: String,
+    state: bool,
+}
+
+impl Thermo {
+    pub fn new(name: impl AsRef<str>, state: bool) -> Self {
+        Self {
+            name: name.as_ref().to_owned(),
+            state,
+        }
     }
 }
 
-impl Device {
-    pub fn new(name: String) -> Self {
-        Self { name }
-    }
-
-    pub fn name(&self) -> &str {
-        &self.name
+impl From<Socket> for DeviceKind {
+    fn from(socket: Socket) -> Self {
+        DeviceKind::Socket(socket)
     }
 }
+
+impl From<Thermo> for DeviceKind {
+    fn from(thermo: Thermo) -> Self {
+        DeviceKind::Thermo(thermo)
+    }
+}
+
+impl_device_info!(Socket, Thermo);
 
 #[cfg(test)]
 mod tests {
-    use crate::device::Device;
-    use crate::device_info::DeviceInfo;
+    use super::*;
 
     #[test]
-    fn test_device_info() {
-        let device = Device::new("smart tv".to_string());
-        assert_eq!(device.name(), "smart tv");
-        assert_eq!(device.state(), "on");
+    fn test_device_socket() {
+        let _socket: DeviceKind = Socket::new("my socket", true).into();
+    }
+
+    #[test]
+    fn test_device_thermo() {
+        let _socket: DeviceKind = Thermo::new("my thermo", true).into();
     }
 }
