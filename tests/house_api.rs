8b3baa3afb436be2b40d13452be4984933feb5f5
ChzenChzen <fedorovdanila@me.com>
2022-06-21 17:24:31 +0800

    complete task

--- tests/house_api.rs
+++ tests/house_api.rs
@@ -2,9 +2,9 @@ use smart_house::*;
 
 #[test]
 fn add_device_to_room_through_house_api() {
-    let mut house: House<DeviceKind> = House::new("White House".to_string());
-    let red = Room::new("Red Room".to_string());
-    let white = Room::new("White Room".to_string());
+    let mut house: House<DeviceKind> = House::new("White House");
+    let red = Room::new("Red Room");
+    let white = Room::new("White Room");
     let socket = Socket::new("Spy socket", true).into();
     let thermo = Thermo::new("Thermo", false).into();
     house.add(red.clone()).unwrap();
@@ -18,15 +18,17 @@ fn add_device_to_room_through_house_api() {
 
 #[test]
 fn remove_device_from_room_through_house_api() {
-    let mut house: House<DeviceKind> = House::new("White House".to_string());
-    let red = Room::new("Red Room".to_string());
-    let white = Room::new("White Room".to_string());
-    let socket = Socket::new("Spy socket", true).into();
+    let mut house = House::new("White House");
+    let red = Room::new("Red Room");
+    let white = Room::new("White Room");
+    let socket: DeviceKind = Socket::new("Spy socket", true).into();
     let thermo = Thermo::new("Thermo", false).into();
     house.add(red.clone()).unwrap();
-    house.add(white.clone()).unwrap();
+    house.add(white).unwrap();
 
-    house.add_device_to_room(socket, red.name()).unwrap();
+    house
+        .add_device_to_room(socket.clone(), red.name())
+        .unwrap();
     house.add_device_to_room(thermo, red.name()).unwrap();
 
     house
@@ -34,5 +36,6 @@ fn remove_device_from_room_through_house_api() {
         .unwrap();
 
     assert_eq!(house.iter().count(), 2);
-    assert_eq!(house.iter().collect::<Vec<_>>()[0].iter().count(), 1);
+    let devices_in_red_room = house.iter_devices_for_room(red.name()).unwrap().count();
+    assert_eq!(devices_in_red_room, 1);
 }
