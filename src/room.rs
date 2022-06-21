101f67ec78b9b7046d59b34c36ee819cc488a32d
ChzenChzen <fedorovdanila@me.com>
2022-06-21 17:09:41 +0800

    refactor `room.rs`

--- src/room.rs
+++ src/room.rs
@@ -1,70 +1,90 @@
-use crate::device::Device;
 use crate::manager::Manager;
+use crate::{DeviceInfo, HouseError};
+use std::collections::HashMap;
 
-impl Manager for Room {
-    type Output = Device;
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Room<T> {
+    name: String,
+    devices: HashMap<String, T>,
+}
+
+impl<T: DeviceInfo> Manager for Room<T> {
+    type Item = T;
 
     fn name(&self) -> &str {
         &self.name
     }
 
-    fn list(&self) -> &[Device] {
-        &self.devices
-    }
-
-    fn add(&mut self, device: Device) {
-        self.devices.push(device);
-    }
-
-    fn remove(&mut self, device: &str) {
-        self.devices.retain(|d| d.name() != device);
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct Room {
-    name: String,
-    devices: Vec<Device>,
-}
-
-impl Room {
-    pub fn new(name: String) -> Self {
-        Self {
-            name,
-            devices: Vec::new(),
+    fn add(&mut self, device: Self::Item) -> Result<(), HouseError> {
+        match self.devices.get(device.name()) {
+            Some(_) => Err(HouseError::DeviceAlreadyExistsError(
+                device.name().to_owned(),
+            )),
+            None => {
+                self.devices.insert(device.name().to_owned(), device);
+                Ok(())
+            }
         }
+    }
+
+    fn remove(&mut self, device_name: impl AsRef<str>) -> Result<(), HouseError> {
+        self.devices.remove(device_name.as_ref()).map_or_else(
+            || {
+                Err(HouseError::DeviceNotFoundError(
+                    device_name.as_ref().to_owned(),
+                ))
+            },
+            |_| Ok(()),
+        )
+    }
+}
+
+impl<T> Room<T> {
+    pub fn new(name: impl AsRef<str>) -> Self {
+        Self {
+            name: name.as_ref().to_owned(),
+            devices: HashMap::new(),
+        }
+    }
+
+    pub(crate) fn iter(&self) -> impl Iterator<Item = &T> {
+        self.devices.values()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{DeviceKind, Socket, Thermo};
 
     #[test]
     fn add_device_to_room() {
-        let mut room = Room::new("Room".to_string());
-        let device = Device::new("Device".to_string());
-        room.add(device);
-        assert_eq!(room.list().len(), 1);
-        assert_eq!(room.list()[0].name(), "Device");
+        let mut room = Room::new("Kitchen");
+        let device = Socket::new("Socket", false);
+        assert_eq!(room.iter().count(), 0);
+        room.add(device).unwrap();
+        let devices: Vec<_> = room.iter().collect();
+        assert_eq!(devices.len(), 1);
+        assert_eq!(devices[0].name(), "Socket");
     }
 
     #[test]
     fn remove_device_from_room() {
-        let mut room = Room::new("Room".to_string());
-        let device = Device::new("Device".to_string());
-        room.add(device);
-        assert_eq!(room.list().len(), 1);
-        room.remove("Device");
-        assert_eq!(room.list().len(), 0);
+        let mut room = Room::new("Kitchen");
+        let device = Thermo::new("Thermo", false);
+        room.add(device).unwrap();
+        assert_eq!(room.iter().count(), 1);
+        room.remove("Thermo").unwrap();
+        assert_eq!(room.iter().count(), 0);
     }
 
     #[test]
     fn list_devices_from_room() {
-        let mut room = Room::new("Room".to_string());
-        let device = Device::new("Device".to_string());
-        room.add(device);
-        assert_eq!(room.list().len(), 1);
-        assert_eq!(room.list()[0].name(), "Device");
+        let mut room: Room<DeviceKind> = Room::new("Kitchen");
+        let socket = Socket::new("Socket", false).into();
+        let thermo = Thermo::new("Thermo", false).into();
+        room.add(thermo).unwrap();
+        room.add(socket).unwrap();
+        assert_eq!(room.iter().count(), 2);
     }
 }
